f99805d3a5b69db8ad64cb6d177ca219763893c4
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-07-26 17:26:11 +0700

    ignore dist

--- apps/server/dist/server/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.site = void 0;
-exports.site = {
-    mamikos: "MAMIKOS",
-    cove: "COVE",
-    rukita: "RUKITA"
-};
-//# sourceMappingURL=constants.js.map
