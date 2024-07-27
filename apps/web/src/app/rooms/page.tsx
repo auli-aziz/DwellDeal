9eda5a9410c161455b570b55b7147cf3fa082c94
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-07-27 10:06:20 +0700

    insert dashboard

--- apps/web/src/app/rooms/page.tsx
+++ apps/web/src/app/rooms/page.tsx
@@ -16,13 +16,12 @@ const Rooms = () => {
           <div className="flex flex-col items-center pt-5 pb-3 font-montserrat text-dark gap-2">
             <h1 className="font-bold lg:text-3xl text-2xl">Rooms</h1>
             <p className="font-regular lg:text-xl text-base text-center">
-              Enter your location to display data visualization and matching
-              rooms
+              Enter your location to display data visualization and matching rooms
             </p>
           </div>
           <SearchBar />
-          <Dashboard/>
-    </section>
+          <Dashboard />
+        </section>
       </RoomProvider>
     </>
   );
