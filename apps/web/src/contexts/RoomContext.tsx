729056f3e72fc1151215e8ed3ba0c4624ddd23e2
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-07-27 14:20:35 +0700

    fix room context

--- apps/web/src/contexts/RoomContext.tsx
+++ apps/web/src/contexts/RoomContext.tsx
@@ -2,6 +2,30 @@ import useFetch from "@web/hooks/useFetch";
 import { fetchResults } from "@web/lib/utils/http";
 import { createContext, FormEvent, ReactNode, useState } from "react";
 
+export interface Result {
+  prices: number[];
+  ratings: number[];
+  highest_price: number;
+  lowest_price: number;
+  highest_rating: number;
+  average_rating: number;
+  total_rooms: number;
+  average_price: number;
+  gender_count: { [key: string]: number };
+  received_data: any[]
+}
+
+interface RoomContextType {
+  keyword: string;
+  result: Result;
+  isLoading: boolean;
+  setKeyword: (keyword: string) => void;
+  handleSubmit: (event: FormEvent) => void;
+}
+
+export const RoomContext = createContext<RoomContextType | undefined>(undefined);
+
+/*
 export const RoomContext = createContext({
   keyword: "",
   result: [""],
@@ -9,6 +33,7 @@ export const RoomContext = createContext({
   setKeyword: (keyword: string) => {},
   handleSubmit: (event: FormEvent) => {}
 });
+
 
 export const RoomProvider = ({ children }: { children: ReactNode }) => {
   const [keyword, setKeyword] = useState<string>("");
@@ -36,3 +61,42 @@ export const RoomProvider = ({ children }: { children: ReactNode }) => {
     </RoomContext.Provider>
   );
 }
+*/
+
+export const RoomProvider = ({ children }: { children: ReactNode }) => {
+  const [keyword, setKeyword] = useState<string>("");
+  const [result, setResult] = useState<Result>({
+    prices: [],
+    ratings: [],
+    highest_price: 0,
+    lowest_price: 0,
+    average_price: 0,
+    average_rating: 0,
+    highest_rating: 0,
+    total_rooms: 0,
+    gender_count: {},
+    received_data: []
+  });
+  const { fetchData, isLoading, error } = useFetch(() => fetchResults(keyword));
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log("clicked");
+    
+    const data = await fetchData();
+
+    if (data) {
+      console.log(data);
+      setResult(data);
+    } else if (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <RoomContext.Provider value={{ keyword, result, isLoading, handleSubmit, setKeyword }}>
+      {children}
+    </RoomContext.Provider>
+  );
+};
