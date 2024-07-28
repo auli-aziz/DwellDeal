3d7442f6defc8cfb546a16768a9ffa86f3f10111
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-07-28 16:40:18 +0700

    add jwt middleware

--- apps/server/src/users/users.module.ts
+++ apps/server/src/users/users.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import User from './user.model';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: User.schema}])
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtService],
 })
 export class UsersModule {}
