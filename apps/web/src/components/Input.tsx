5762ed13c1f1e2851b09bcc4324a8ae0db1c91f7
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-07-30 23:23:28 +0700

    use useref

--- apps/web/src/components/Input.tsx
+++ apps/web/src/components/Input.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
-const Input = ({ title, name, type, placeholder }: { title: string, name: string, type: string, placeholder: string }) => {
+interface Props {
+  title: string;
+  name: string;
+  type: string;
+  placeholder: string;
+}
+
+const Input = forwardRef<HTMLInputElement, Props>(function Input({
+  title,
+  name,
+  type,
+  placeholder,
+}, ref) {
   return (
     <div className="flex flex-col w-full h-fit font-shanti mb-5">
       <label htmlFor="username" className="mb-2 text-base font-medium">
@@ -10,11 +22,12 @@ const Input = ({ title, name, type, placeholder }: { title: string, name: string
         type={type}
         name={name}
         placeholder={placeholder}
+        ref={ref}
         required
         className="font-regular text-sm rounded-3xl p-3 border-2 border-primary"
       />
     </div>
   );
-};
+});
 
 export default Input;
