01e169571017bfb7c11714ee46f99f70a16e1a1c
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-07-30 10:26:02 +0700

    layouting dashboard ( - room proportion chart)

--- apps/web/src/components/rooms/dashboard/Dashboard.tsx
+++ apps/web/src/components/rooms/dashboard/Dashboard.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { Box, Grid, Paper } from "@mui/material";
-import scss from "./Dashboard.module.scss"
-import DataRibbon from "./dataribbon/DataRibbon";
-import HighestPriceChart from "./highestprice/HighestPriceChart";
+import DataRibbon from "./components/dataribbon/DataRibbon";
+import HighestPriceChart from "./components/HighestPriceChart";
+import HighestRating from "./components/HighestRating";
+import LowestPriceChart from "./components/LowestPriceChart";
 
 const Dashboard = () => {
     return (
-        <div className="bg-neutral rounded-xl p-10">
-            <Box display="flex" justifyContent="space-between" alignItems="flex-start" gap={2}>
-                <Box flex={1} mr={2}>
+        <div className="bg-neutral rounded-xl p-10 mb-10">
+            <Box display="flex" justifyContent="space-between" alignItems="flex-start" gap={2} mb={3}>
+                <Box flex={1} mr={1}>
                     <HighestPriceChart />
                 </Box>
                 <Box flex={1}>
                     <DataRibbon />
+                </Box>
+            </Box>
+            <Box display="flex" justifyContent="space-between" alignItems="flex-start" gap={2}>
+                <Box flex={1} mr={2}>
+                    <HighestPriceChart />
+                </Box>
+                <Box display="flex" flexDirection="column" justifyContent="space-between" alignItems="flex-start" gap={2}>
+                    <Box flex={1} mb={2}>
+                        <LowestPriceChart />
+                    </Box>
+                    <Box flex={1}>
+                        <HighestRating />
+                    </Box>
                 </Box>
             </Box>
         </div>
