46a8746186b176ec8e1700db382273b835513786
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-08-30 23:15:48 +0700

    feat: add reset password UI

--- apps/web/src/app/settings/page.tsx
+++ apps/web/src/app/settings/page.tsx
@@ -9,7 +9,7 @@ const AccountSettings = async () => {
   
 
   return (
-    <>
+    <div className="flex flex-col">
       <div className="flex gap-5">
         <IoPersonCircleSharp className="text-primary text-[195px]" />
         <div className="w-[600px] flex flex-col gap-3 justify-center text-dark">
@@ -17,8 +17,17 @@ const AccountSettings = async () => {
           <p className="font-thin text-xl">{session?.user.email}</p>
         </div>
       </div>
-      <div></div>
-    </>
+      <div className={"mt-5 p-5 w-full rounded-lg"}>
+        <h3 className="font-montserrat font-bold text-dark text-xl">Account Settings</h3>
+        <form method="POST" className="flex flex-col">
+          <label htmlFor="">Current Password</label>
+          <input type="password" className="border-2 rounded-xl" />
+          <label htmlFor="">Reset Password</label>
+          <input type="password" className="border-2 rounded-xl" />
+          <button type="submit" className="w-[150px] p-2 mt-10 bg-secondary text-white text-lg rounded-xl">Save</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
