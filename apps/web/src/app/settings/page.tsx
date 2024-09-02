9b79f32c0f2b03301572d0cfe9341feafa6a1e77
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-09-02 23:17:02 +0700

    refactor: modularize input

--- apps/web/src/app/settings/page.tsx
+++ apps/web/src/app/settings/page.tsx
@@ -1,12 +1,14 @@
 import { getServerSession } from "next-auth";
-import React from "react";
+import React, { useRef } from "react";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import Input from "@web/components/Input";
 
 const AccountSettings = async () => {
   const session = await getServerSession(authOptions);
   console.log(session);
-  
+  const newPassRef = useRef<HTMLInputElement>(null);
+  const confirmPassRef = useRef<HTMLInputElement>(null);
 
   return (
     <div className="flex flex-col">
@@ -20,10 +22,8 @@ const AccountSettings = async () => {
       <div className={"mt-5 p-5 w-full rounded-lg"}>
         <h3 className="font-montserrat font-bold text-dark text-xl">Account Settings</h3>
         <form method="POST" className="flex flex-col">
-          <label htmlFor="">Current Password</label>
-          <input type="password" className="border-2 rounded-xl" />
-          <label htmlFor="">Reset Password</label>
-          <input type="password" className="border-2 rounded-xl" />
+          <Input title="New Password" type="password" placeholder="" name="newpassword" ref={newPassRef} />
+          <Input title="Confirm Password" type="password" placeholder="" name="confirmpassword" ref={confirmPassRef} />
           <button type="submit" className="w-[150px] p-2 mt-10 bg-secondary text-white text-lg rounded-xl">Save</button>
         </form>
       </div>
